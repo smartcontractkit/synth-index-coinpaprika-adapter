bb14cd881adb0bd20cb0af935ecaf366cec96f23
Thomas Hodges <me@thodges.com>
2020-03-17 15:23:47 -0500

    Handle errors from API and in index calculation

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ CoinPaprika, and calculates a weighted index.
 ## Install
 
 ```bash
-yarn install
+yarn
 ```
 
 ## Test
@@ -26,15 +26,12 @@ zip -r cl-synth-index-coinpaprika.zip .
 - In Lambda Functions, create function
 - On the Create function page:
   - Give the function a name
-  - Use Node.js 8.10 for the runtime
+  - Use Node.js 12.x for the runtime
   - Choose an existing role or create a new one
   - Click Create Function
 - Under Function code, select "Upload a .zip file" from the Code entry type drop-down
 - Click Upload and select the `cl-synth-index-coinpaprika.zip` file
 - Handler should remain index.handler
-- Add the environment variable (repeat for all environment variables):
-  - Key: API_KEY
-  - Value: Your_API_key
 - Save
 
 
@@ -44,6 +41,3 @@ zip -r cl-synth-index-coinpaprika.zip .
 - Click Browse and select the `cl-synth-index-coinpaprika.zip` file
 - Select a Storage Bucket to keep the zip in
 - Function to execute: gcpservice
-- Click More, Add variable (repeat for all environment variables)
-  - NAME: API_KEY
-  - VALUE: Your_API_key
--- index.js
+++ index.js
@@ -1,76 +1,82 @@
-const rp = require('request-promise');
-const snx = require('synthetix');
-const Decimal = require('decimal.js');
+const rp = require('request-promise')
+const snx = require('synthetix')
+const Decimal = require('decimal.js')
 
 const getPriceData = async () => {
-    return rp({
-        url: `https://api.coinpaprika.com/v1/tickers`,
-        qs: {
-            quotes: "USD"
-        },
-        json: true
+  try {
+    return await rp({
+      url: `https://api.coinpaprika.com/v1/tickers`,
+      qs: {
+        quotes: 'USD'
+      },
+      json: true
     })
-};
+  } catch (error) {
+    throw error.message
+  }
+}
 
 const calculateIndex = (indexes) => {
-    let value = new Decimal(0);
+  let value = new Decimal(0)
+  try {
     indexes.forEach(i => {
-        const price = i.priceData.quotes.USD.price;
-        if (price <= 0)
-            throw "invalid price";
-        value = value.plus(new Decimal(i.units).times(new Decimal(price)));
-    });
-    return value.toNumber()
-};
+      const price = i.priceData.quotes.USD.price
+      if (price <= 0)
+        throw 'invalid price'
+      value = value.plus(new Decimal(i.units).times(new Decimal(price)))
+    })
+  } catch (error) {
+    throw error.message
+  }
+  return value.toNumber()
+}
 
 const createRequest = async (input, callback) => {
-    const asset = input.data.asset || 'sCEX';
-    const datas = snx.getSynths({network: 'mainnet'}).filter(({index, inverted}) => index && !inverted);
-    const data = datas.find(d => d.name.toLowerCase() === asset.toLowerCase());
-    const priceDatas = await getPriceData();
+  const asset = input.data.asset || 'sCEX'
+  const datas = snx.getSynths({network: 'mainnet'}).filter(({index, inverted}) => index && !inverted)
+  const data = datas.find(d => d.name.toLowerCase() === asset.toLowerCase())
+  try {
+    const priceDatas = await getPriceData()
     await Promise.all(data.index.map(async (synth) => {
-        synth.priceData = priceDatas.find(d => d.symbol.toLowerCase() === synth.symbol.toLowerCase())
-    }));
-
-    try {
-        data.result = calculateIndex(data.index);
-    } catch (e) {
-        callback(500, {
-            jobRunID: input.id,
-            error: "failed getting price",
-            statusCode: 500
-        });
-        return
-    }
-
+      synth.priceData = priceDatas.find(d => d.symbol.toLowerCase() === synth.symbol.toLowerCase())
+    }))
+    data.result = calculateIndex(data.index)
     callback(200, {
-        jobRunID: input.id,
-        data: data,
-        result: data.result,
-        statusCode: 200
+      jobRunID: input.id,
+      data: data,
+      result: data.result,
+      statusCode: 200
     })
-};
+  } catch (error) {
+    callback(500, {
+      jobRunID: input.id,
+      status: 'errored',
+      error: error,
+      statusCode: 500
+    })
+  }
+}
 
 exports.gcpservice = (req, res) => {
-    createRequest(req.body, (statusCode, data) => {
-        res.status(statusCode).send(data)
-    })
-};
+  createRequest(req.body, (statusCode, data) => {
+    res.status(statusCode).send(data)
+  })
+}
 
 exports.handler = (event, context, callback) => {
-    createRequest(event, (statusCode, data) => {
-        callback(null, data)
-    })
-};
+  createRequest(event, (statusCode, data) => {
+    callback(null, data)
+  })
+}
 
 exports.handlerv2 = (event, context, callback) => {
-    createRequest(JSON.parse(event.body), (statusCode, data) => {
-        callback(null, {
-            statusCode: statusCode,
-            body: JSON.stringify(data),
-            isBase64Encoded: false
-        })
+  createRequest(JSON.parse(event.body), (statusCode, data) => {
+    callback(null, {
+      statusCode: statusCode,
+      body: JSON.stringify(data),
+      isBase64Encoded: false
     })
-};
+  })
+}
 
-module.exports.createRequest = createRequest;
+module.exports.createRequest = createRequest
